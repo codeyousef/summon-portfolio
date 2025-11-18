323f613dde62c6defc866f525139dc61190e62e7
codeyousef <yousef.baitalmal@gmail.com>
2025-11-18 22:28:01 +0300

    fix: add wasm-polyfill.js to define wasmConsoleLog

--- src/main/kotlin/code/yousef/portfolio/ssr/BlogRenderer.kt
+++ src/main/kotlin/code/yousef/portfolio/ssr/BlogRenderer.kt
@@ -74,6 +74,7 @@ class BlogRenderer(
         head.link("canonical", canonical, null, null, null, null)
         head.link("alternate", blogCanonical(PortfolioLocale.EN), "en", null, null, null)
         head.link("alternate", blogCanonical(PortfolioLocale.AR), "ar", null, null, null)
+        head.script("/static/wasm-polyfill.js", "wasm-polyfill", "application/javascript", false, false, null)
         head.script(HYDRATION_SCRIPT_PATH, "summon-hydration-runtime", "application/javascript", false, false, null)
     }
 
@@ -97,6 +98,7 @@ class BlogRenderer(
         head.link("canonical", canonical, null, null, null, null)
         head.link("alternate", blogDetailCanonical(PortfolioLocale.EN, slug), "en", null, null, null)
         head.link("alternate", blogDetailCanonical(PortfolioLocale.AR, slug), "ar", null, null, null)
+        head.script("/static/wasm-polyfill.js", "wasm-polyfill", "application/javascript", false, false, null)
         head.script(HYDRATION_SCRIPT_PATH, "summon-hydration-runtime", "application/javascript", false, false, null)
     }
 
---
+++ src/main/resources/static/wasm-polyfill.js
@@ -0,0 +1,3 @@
+window.wasmConsoleLog = function(message) {
+    console.log("[WASM]", message);
+};
