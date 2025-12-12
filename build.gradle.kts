e5628fd1bae8ae5c5857a4a4d7dfae08c572a3c3
yousef.code <yousef.baitalmal@gmail.com>
2025-12-12 09:49:03 +0300

    Upgrade to Sigil 0.2.7.2 - proper JSON escaping for script tags

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,11 +46,11 @@ dependencies {
     implementation("org.jsoup:jsoup:1.18.1")
 
     // Summon SSR framework + Sigil 3D/effects library
-    // Sigil 0.2.7.1 - Fixed JSON parsing in hydration + auto-loading hydration bundle
+    // Sigil 0.2.7.2 - Proper JSON escaping for script tags vs HTML attributes
     implementation("codes.yousef:summon:0.5.8.4")
     implementation("codes.yousef:materia-jvm:0.3.3.0")
-    implementation("codes.yousef.sigil:sigil-schema-jvm:0.2.7.1")
-    implementation("codes.yousef.sigil:sigil-summon-jvm:0.2.7.1")
+    implementation("codes.yousef.sigil:sigil-schema-jvm:0.2.7.2")
+    implementation("codes.yousef.sigil:sigil-summon-jvm:0.2.7.2")
     implementation("ch.qos.logback:logback-classic:$logback_version")
 
     // Cloud Firestore (ready for future integrations)
